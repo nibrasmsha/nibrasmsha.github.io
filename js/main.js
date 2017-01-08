f033f9cc835f1641d214c5a8b835b78de630ec73
nibrasmsha <Nibras@Nibrass-MacBook-Pro.local>
2017-01-08 01:24:11 -0500

    First Website Upload

---
+++ js/main.js
@@ -0,0 +1,42 @@
+
+$(document).ready(function() {
+	var height = $(window).height();
+
+    $('#header').css('height', height);
+	   
+
+    //Scrollspy plugin
+    $('body').scrollspy({ target: '.navbar-spy'});
+
+
+});
+function fadeNav(){  
+  var offset = getScrollXY();
+  //if y offset is greater than 0, set opacity to desired value, otherwise set to 1
+  offset[1] > 0 ? setNavOpacity(0.5) : setNavOpacity(1.0); 
+}
+
+function setNavOpacity(newOpacity){
+  var navBar = document.getElementById("navbar");
+  navBar.style.opacity = newOpacity;
+}
+
+function getScrollXY() {
+  var scrOfX = 0, scrOfY = 0;
+if( typeof( window.pageYOffset ) == 'number' ) {
+  //Netscape compliant
+  scrOfY = window.pageYOffset;
+  scrOfX = window.pageXOffset;
+} else if( document.body && ( document.body.scrollLeft || document.body.scrollTop ) ) {
+  //DOM compliant
+  scrOfY = document.body.scrollTop;
+  scrOfX = document.body.scrollLeft;
+} else if( document.documentElement && ( document.documentElement.scrollLeft ||     
+  document.documentElement.scrollTop ) ) {
+  //IE6 standards compliant mode
+  scrOfY = document.documentElement.scrollTop;
+  scrOfX = document.documentElement.scrollLeft;
+}
+
+  return [ scrOfX, scrOfY ];
+}
